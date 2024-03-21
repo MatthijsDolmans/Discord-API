3c9e90ca9c05f0f71e7ae6b60c941fa276dc329d
matthijs Dolmans <487249@student.fontys.nl>
2024-03-21 15:38:09 +0100

    test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,4 @@
-const path = require('path');
-
-module.exports = {
-  transpileDependencies: true,
-  chainWebpack: config => {
-    config.plugin('copy').tap(args => {
-      args[0].push({
-        from: path.resolve(__dirname, 'public/_redirects'),
-        to: path.resolve(__dirname, 'dist'),
-        toType: 'file',
-        ignore: ['.*']
-      });
-      return args;
-    });
-  }
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
