01d8ade15da0fbd95625fdd93c008bde69f514c1
matthijs Dolmans <487249@student.fontys.nl>
2024-03-21 15:36:38 +0100

    x

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,17 +2,15 @@ const path = require('path');
 
 module.exports = {
   transpileDependencies: true,
-  chainWebpack: (config) => {
-    config
-      .plugin('copy')
-      .tap((args) => {
-        args[0].push({
-          from: path.resolve(__dirname, 'public/_redirects'), 
-          to: path.resolve(__dirname, 'dist'), 
-          toType: 'file',
-          ignore: ['.*'],
-        });
-        return args;
+  chainWebpack: config => {
+    config.plugin('copy').tap(args => {
+      args[0].push({
+        from: path.resolve(__dirname, 'public/_redirects'),
+        to: path.resolve(__dirname, 'dist'),
+        toType: 'file',
+        ignore: ['.*']
       });
-  },
+      return args;
+    });
+  }
 };
