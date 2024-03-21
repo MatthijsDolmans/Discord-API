4b72574491b4f360d2f8e91a7cd5f3af4e9df572
matthijs Dolmans <487249@student.fontys.nl>
2024-03-21 15:26:21 +0100

    redirects 2

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+
+module.exports = {
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config
+      .plugin('copy')
+      .tap((args) => {
+        args[0].push({
+          from: path.resolve(__dirname, 'public/_redirects'), // Adjust the path as necessary
+          to: path.resolve(__dirname, 'dist'), // Adjust the output directory as necessary
+          toType: 'file',
+          ignore: ['.*'],
+        });
+        return args;
+      });
+  },
+};
